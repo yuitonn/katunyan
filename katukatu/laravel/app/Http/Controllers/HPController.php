560f8d85755eb93ee4f97d302259f7a220c18497
yuito <yuito.yoshikawa@gmail.com>
2024-11-14 04:53:28 +0900

    ok

--- katukatu/laravel/app/Http/Controllers/HPController.php
+++ katukatu/laravel/app/Http/Controllers/HPController.php
@@ -6,14 +6,14 @@ use Illuminate\Http\Request;
 use App\Models\User;
 class HPController extends Controller
 {
-    public function reduceHP($userId)
+    public function reduceHP($id)
     {
-        $user = User::find($userId);
+        $user = User::find($id);
         $user->hp = max(0, $user->hp - 10); // HPを10減少（0未満にはならない）
         $user->save();
 
         // HP変更イベントをブロードキャスト
-        event(new HPChanged($userId, $user->hp));
+        event(new HPChanged($id, $user->hp));
 
         return response()->json(['hp' => $user->hp]);
     }
--- katukatu/laravel/resources/js/bootstrap.js
+++ katukatu/laravel/resources/js/bootstrap.js
@@ -9,6 +9,15 @@ window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.Pusher = require("pusher-js");
+
+window.Echo = new Echo({
+    broadcaster: "pusher",
+    key: "your app key",
+    cluster: "ap3",
+    encrypted: true
+    });
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
