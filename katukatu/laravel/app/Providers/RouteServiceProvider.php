ebad09569a7e6012e2251ba17e29553156ce1fe5
yuito <yuito.yoshikawa@gmail.com>
2024-11-14 01:55:30 +0900

    ok

--- katukatu/laravel/app/Providers/RouteServiceProvider.php
+++ katukatu/laravel/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/reduce-hp/{userId}';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
